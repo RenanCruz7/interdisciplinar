f76dac217ab4fa36c1c9fdb46b5477669c5dc4f1
RenanCruz7 <renancruz199@gmail.com>
2022-11-23 21:46:39 -0300

    Login

---
+++ src/componentes/Login/index.js
@@ -0,0 +1,27 @@
+import './style.css'
+import logo from '../../imagens/mangayakiLogo.png'
+import styled from 'styled-components';
+
+
+function Login(){
+    return(
+        <body>
+            <img src={logo} alt='logo' className='Logo'></img>
+            <section className='area-login'>
+                    <div className='login'>
+                        <form>
+                            <h2>Login</h2>
+                            <input type='text' name='nome' placeholder='Nome de usuario ou email' autoFocus></input>
+                            <h2>Senha</h2>
+                            <input type='password' name='senha' placeholder='Senha'></input><br></br>
+                            <p><a href='#'>Esqueceu sua Conta</a></p>
+                            <input type='submit' value='Entrar'></input>
+                            <input type='submit' value='Criar uma nova conta'></input>
+                        </form>
+                    </div>
+            </section>
+        </body>
+    )
+}
+
+export default Login
